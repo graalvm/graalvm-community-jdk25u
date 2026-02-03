126e6f93860e6769809ff3d51099737dd3dd0b93
Robert Toyonaga <rtoyonag@redhat.com>
2026-02-03 15:24:57 -0500

    Fix Thread.dump_to_file.
    (cherry picked from commit d2e76d02432b8f01eb89d6e844fc2b7736ac25ed)
    
    fix
    
    whitespace

--- substratevm/mx.substratevm/suite.py
+++ substratevm/mx.substratevm/suite.py
@@ -1129,6 +1129,7 @@ suite = {
             "requiresConcealed" : {
                 "java.base" : [
                     "jdk.internal.misc",
+                    "jdk.internal.vm",
                     "sun.security.jca",
                 ],
             },
